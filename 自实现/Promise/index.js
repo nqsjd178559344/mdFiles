c660bbaf8672562f20136a89cd9a47bf0b9dd2f0
nqsjd178559344 <1401444513@qq.com>
2021-08-24 14:00:16 +0800

    feat: update promise

--- 自实现/Promise/index.js
+++ 自实现/Promise/index.js
@@ -1,67 +1,123 @@
 class MyPromise {
     constructor(callback) {
         this._status = 'pending'
-        this.s_res = null
-        this.f_res = null
-        this._query = []
-        callback(value => {
-            this.s_res = value
+        this._s_res = null
+        this._f_res = null
+        this._unfinished_events = []
+        callback(arg => {
             this._status = 'fulfilled'
-            this._query.forEach(item => {
-                item.resolve(value)
+            this._s_res = arg
+            this._unfinished_events.forEach(item => {
+                const returnValue =  item.resolve(arg) || arg
+                handle(returnValue, item.resFn, item.rejFn)
             })
-        }, value => {
-            this.f_res = value
-            this._status = 'rejected'
-            this._query.forEach(item => {
-                item.reject(value)
+        }, arg => {
+            this._status = 'reject'
+            this._f_res = arg
+            this._unfinished_events.forEach(item => {
+                const returnValue =  item.reject(arg)
+                handle(returnValue, item.resFn, item.rejFn)
             })
         })
     }
     then(onResolve, onReject) {
-        if (this._status === 'fulfilled') {
-            onResolve(this.s_res)
-            return
-        } else if (this._status === 'rejected') {
-            onReject(this.f_res)
-        } else {
-            this._query.push({
-                resolve: onResolve,
-                reject: onReject,
-            })
-        }
+        return new MyPromise((resFn, rejFn) => {
+            if (this._status === 'fulfilled') {
+                const returnValue = onResolve(this._s_res) || this._s_res
+                handle(returnValue, resFn, rejFn)
+            } else if (this._status === 'reject') {
+                const returnValue = onReject(this._f_res)
+                errorBack(returnValue, resFn, rejFn)
+            } else {
+                this._unfinished_events.push({
+                    resolve: onResolve,
+                    reject: onReject,
+                    resFn,
+                    rejFn
+                })
+            }
+        })
+
+    }
+
+}
+
+function handle(returnValue, resFn, rejFn) {
+    if (returnValue && returnValue['then'] && typeof returnValue['then'] === 'function') {
+        returnValue.then(success => {
+            resFn(success)
+        }, error => {
+            rejFn(error)
+        })
+    } else {
+        resFn(returnValue)
     }
 }
 
-// todo basic
-// new MyPromise((resolve, reject) => {
-//     resolve(1);
-// }).then(res => {
-//     console.log(res,'MyPromise_success'); //1
-// },error=>{
-//     console.log(error,'MyPromise_error'); //2
-// });
+function errorBack(returnValue, resFn, rejFn) {
+    if (returnValue && returnValue['then'] && typeof returnValue['then'] === 'function') {
+        returnValue.then(success => {
+            resFn(success)
+        }, error => {
+            rejFn(error)
+        })
+    } else {
+        resFn(returnValue)
+    }
+}
 
-// todo 异步处理
-new MyPromise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(2333);
-        // reject(2333);
-    }, 1000);
-}).then(res => {
-    console.log(res, 'MyPromise_success');
-}, error => {
-    console.log(error, 'MyPromise_error'); //2
-});
-
-// // todo 处理链式调用
-// new MyPromise((resolve, reject) => {
-//     resolve(1);
+// todo 基础使用
+// new MyPromise((resolve,reject)=>{
+//     resolve(1)
+// }).then(res=>{
+//     console.log(res,'res')
 // })
-//     .then(res => {
-//         return res;
-//     })
-//     .then(res => {
-//         console.log(res);
-//     });
 
+// new MyPromise((resolve,reject)=>{
+//     reject(1)
+// }).then(res=>{
+//     console.log(res,'res')
+// },error=>{
+//     console.log(error,'error')
+// })
+
+// todo 处理异步
+// new MyPromise((resolve, reject) => {
+//     setTimeout(function () {
+//         resolve(1)
+//     }, 1000)
+// }).then(res => {
+//     console.log(res, 'res')
+// })
+
+// todo 链式调用
+// new MyPromise((resolve, reject) => {
+//     resolve(1)
+// })
+// .then(res => {
+//     return res + 233
+// })
+// .then(res => {
+//     return res + '111'
+// })
+// .then(res => {
+//     console.log(res, 'res')
+// })
+
+new MyPromise((resolve, reject) => {
+    setTimeout(function () {
+        resolve(7)
+    }, 1000)
+})
+.then(res => {
+    return res + 1
+})
+.then(res => {
+    return res + 2
+})
+.then(res => {
+    return res + '111'
+})
+.then(res => {
+    console.log(res, 'res')
+})
