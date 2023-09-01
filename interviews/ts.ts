51fbc503dbeafee9ca9e6de1170cc1c2cb25e962
nqsjd178559344 <nqsjd178559344@163.com>
2023-09-01 18:51:27 +0800

    feat: ts

--- interviews/ts.ts
+++ interviews/ts.ts
@@ -11,22 +11,3 @@ type A1 = Extract<A, C>; // "b" | "c"
 
 // Exclude<'b',E2> | Exclude<'c',E2>
 type Result = Exclude<A1, E2>;
-
-export type Equals1<T, S> = [T] extends [S]
-  ? [S] extends [T]
-    ? true
-    : false
-  : false;
-
-type Equals1Test = Equals1<any, 1>;
-
-// https://github.com/microsoft/TypeScript/issues/27024
-export type Equals2<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
-  T
->() => T extends Y ? 1 : 2
-  ? true
-  : false;
-
-type Equals2Test1 = Equals1<any, 1>;
-type Equals2Test2 = Equals1<any, any>;
-type Equals2Test3 = Equals1<never, never>;
