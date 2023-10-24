9b13a5458c385892ede09a87181ecd953906dbbd
nqsjd178559344 <nqsjd178559344@163.com>
2023-10-24 10:46:11 +0800

    feat: 柯里化2

--- 自实现/柯里化/index.js
+++ 自实现/柯里化/index.js
@@ -1,46 +1,72 @@
-// ? 1. 固定参数自动调用
 function add(a, b, c) {
-    return a + b + c
+  return a + b + c;
 }
+/**
+    length是函数对象的一个属性值,指该函数有多少个必须要传入的的参数,即形参
+    的个数。
+    形参的数量不包括剩余参数个数,仅包括第一个具有默认值之前的参数个数。
+    与之对比的是,arguments.length是函数被调用时实际传参的个数。
+ */
+// todo 到位数后自动调用
 function currying1(fn) {
-    let args = []
-    return function () {
-        Array.prototype.push.apply(args, arguments)
-        if (args.length === fn.length) { // 调用
-            return fn.apply(fn, args)
-        } else {
-            return arguments.callee
-        }
+  let args = [];
+  const f = function (...arg) {
+    args.push(...arg);
+    const flag = fn.length === args.length;
+    if (flag) {
+      return fn(...args);
+    } else {
+      return f;
     }
+  };
+
+  return f;
 }
 
-// todo test
-// const curry = currying1(add)
-// let res1 = curry(1)(2)
-// let res2 = curry(2)(3)
-// let res3 = curry(233)(344)
-// let res4 = curry(233)(344)(555)
+let res1 = currying1(add)(1)(2)(3);
+let res2 = currying1(add)(2)(3);
+let res3 = currying1(add)(233)(344);
+let res4 = currying1(add)(233)(344)(555);
 
-// console.log(res1)
-// console.log(res2)
-// console.log(res4)
+// console.log(res1);
+// console.log(res2);
+// console.log(res4);
 
-//? 2. 自己将其调用
+// todo 手动调用
 function currying2(fn) {
-    let args = []
-    return function () {
-        if (!arguments.length) { // 调用
-            return fn.apply(fn, args)
-        } else {
-            Array.prototype.push.apply(args, arguments)
-            return arguments.callee
-        }
+  let args = [];
+  const f = function (...arg) {
+    if (!arg.length) {
+      return fn(...args);
+    } else {
+      args.push(...arg);
+      return f;
     }
+  };
+  return f;
 }
-const curry = currying2(add)
-// let res5 = curry(233)(344)(246)()
-// console.log(res5)
+let res5 = currying2(add)(233)(344)(246)();
+console.log(res5);
+let res6 = currying2(add)(233)(344)(423)(234)();
+console.log(res6);
 
-let res6 = curry(233)(344)(423)(234)()
-console.log(res6)
+// todo 如何实现一个无限累加的 sum 函数
+// const res1 = sum(1, 2, 3).valueOf(); //6
+// const res2 = sum(2, 3)(2).valueOf(); //7
+// const res3 = sum(1)(2)(3)(4).valueOf(); //10
+// const res4 = sum(2)(4, 1)(2).valueOf(); //9
+// const res5 = sum(1)(2)(3)(4)(5)(6).valueOf(); // 21
 
+function sum(...args) {
+  const f = function (...values) {
+    return sum(...args, ...values);
+  };
+
+  f.valueOf = function () {
+    return args.reduce((p, c) => {
+      return p + c;
+    }, 0);
+  };
+
+  return f;
+}
