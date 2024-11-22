3086557d0fe446a8d5b3f40fdd2825845035b9e4
nqsjd178559344 <nqsjd178559344@163.com>
2024-11-22 13:44:33 +0800

    feat: 递归

--- interviews/递归/productOfArray.js
+++ interviews/递归/productOfArray.js
@@ -3,6 +3,8 @@
 const arr = [2, 3, 4];
 function productOfArray(arr) {
   // 实现代码
+  if (!arr.length) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
 }
 console.log(productOfArray(arr)); // 输出 24
 
