534b7e4bd07f4d8b43f31614963e5e114a628c75
nqsjd178559344 <nqsjd178559344@163.com>
2025-02-25 14:30:15 +0800

    feat：SSE与websocket的区别 & SSE demo

---
+++ markdown/SSE/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+// 修改静态文件服务的路径配置
+app.use(express.static(path.join(__dirname)));
+
+// 模拟AI响应生成
+function generateAIResponse(message) {
+  const responses = [
+    "思考中...",
+    "正在处理您的问题...",
+    "让我想想...",
+    `对于"${message}"，我的回答是...`,
+    "希望这个回答对您有帮助！",
+  ];
+  return responses;
+}
+
+// 处理SSE响应的公共函数
+function handleSSEResponse(res, responses) {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  let index = 0;
+  const interval = setInterval(() => {
+    if (index < responses.length) {
+      res.write(`data: ${JSON.stringify({ text: responses[index] })}\n\n`);
+      index++;
+    } else {
+      clearInterval(interval);
+      res.end();
+    }
+  }, 1000);
+
+  res.on("close", () => {
+    clearInterval(interval);
+  });
+}
+
+// TODO 处理GET聊天消息
+// app.get("/chat", (req, res) => {
+//   const message = req.query.message;
+//   const responses = generateAIResponse(message);
+
+//   handleSSEResponse(res, responses);
+// });
+
+// TODO 处理POST聊天消息
+app.post("/chat", (req, res) => {
+  const message = req.body.message;
+  const responses = generateAIResponse(message);
+
+  handleSSEResponse(res, responses);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
